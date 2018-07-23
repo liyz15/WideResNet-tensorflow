d94089627546c58ca8438e4eaa72c0dc114f0f5a
Yizhuo Li <yzli@yitu-inc.intra>
2018-07-23 14:12:20 +0800

    [Bug Fix] Fixed weight decay

--- main.py
+++ main.py
@@ -23,15 +23,18 @@ def data_preprocess(is_training=True):
 
 def model_fn(features, labels, mode, params):
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
+
     def get_lr():
         assert len(params['lr_boundaries']) + 1 == len(params['lr_values']), 'The learning_rate setting is incorrect'
-        global_step = tf.train.get_global_step()
-        return tf.train.piecewise_constant(global_step, params['lr_boundaries'], params['lr_values'])
+        return tf.train.piecewise_constant(tf.train.get_global_step(), params['lr_boundaries'], params['lr_values'])
 
     with tf.variable_scope('net'):
         logits = wide_residual_network(features, is_training, params)
     with tf.variable_scope('loss'):
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=logits, name='loss'))
+        with tf.variable_scope('weight_decay'):
+            regular_loss = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
+            loss = loss + params['weight_decay'] * regular_loss
     with tf.variable_scope('predictions'):
         predictions = tf.argmax(tf.nn.softmax(logits=logits), axis=-1, name='predictions')
     with tf.variable_scope('optimizer'):
@@ -42,7 +45,7 @@ def model_fn(features, labels, mode, params):
     with tf.variable_scope('accuracy'):
         accuracy = tf.metrics.accuracy(tf.argmax(labels, axis=-1), predictions)
 
-    logging_hook = tf.train.LoggingTensorHook({'loss' : loss, 'accuracy': accuracy[1]}, every_n_iter=100)
+    logging_hook = tf.train.LoggingTensorHook({'loss' : loss, 'accuracy': accuracy[1], 'lr': get_lr()}, every_n_iter=100)
 
     return tf.estimator.EstimatorSpec(mode, predictions, loss, train_op, eval_metric_ops={'accuracy': accuracy},
             training_hooks = [logging_hook])
@@ -55,7 +58,8 @@ params = {'depth': 28,
           'batch_size': 128,
           'lr_boundaries': [28125, 56250, 75000],
           'lr_values': [0.1, 2e-2, 4e-3, 8e-4],
-          'momentum': 0.9}
+          'momentum': 0.9,
+          'weight_decay': 5e-4}
 (x_train, y_train), (x_test, y_test) = tf.contrib.keras.datasets.cifar100.load_data(label_mode='fine')
 
 train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -91,6 +95,7 @@ model = tf.estimator.Estimator(model_fn=model_fn,
                                params=params)
 
 model.evaluate(input_fn=input_fn(is_training=False))
-for _ in range(90):
-    model.train(input_fn=input_fn(is_training=True), steps=1000)
+for _ in range(18):
+    model.train(input_fn=input_fn(is_training=True), steps=5000)
     model.evaluate(input_fn=input_fn(is_training=False))
+
